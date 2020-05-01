ebaab9ba18b06fc9dadbd7bae5c695dbeb97a762
Polk Farody <polkfarody@gmail.com>
2020-05-01 17:37:56 +1000

    ST-3 Update project model
    - Added two new fields to the project model
    - Made project model implement SoftDeletModel and TimestampedModel
    - Update seriliazer
    - Add cors middleware

--- stitch-it-all-together/projects/models.py
+++ stitch-it-all-together/projects/models.py
@@ -1,27 +1,25 @@
 from django.db import models
 
 from django.db import models
+from softdelete.models import SoftDeleteModel
 
-TYPES = sorted([
-    (1, 'Music'),
-    (2, 'Lyrics'),
-    (3, 'Joke'),
-    (4, 'Story'),
-])
+from core.models import TimestampedModel
 
 
-class Project(models.Model):
+class Project(SoftDeleteModel, TimestampedModel):
+    PROJECT_TYPES = sorted([
+        (1, 'Music'),
+        (2, 'Lyrics'),
+        (3, 'Joke'),
+        (4, 'Story'),
+    ])
+
     title = models.CharField(max_length=100)
     description = models.TextField(blank=True, default='')
-    type = models.SmallIntegerField(choices=TYPES, default=1)
-
+    type = models.SmallIntegerField(choices=PROJECT_TYPES, default=1)
+    max_stitches = models.PositiveSmallIntegerField(null=True, default=None)
+    is_private = models.BooleanField(default=False)
     owner = models.ForeignKey('stitchers.Stitcher', related_name='projects', on_delete=models.CASCADE)
-
-    created = models.DateTimeField(auto_now_add=True)
-    modified = models.DateTimeField(auto_now=True)
 
     def save(self, *args, **kwargs):
         super(Project, self).save(*args, **kwargs)
-
-    class Meta:
-        ordering = ['created']
--- stitch-it-all-together/projects/serializers.py
+++ stitch-it-all-together/projects/serializers.py
@@ -5,7 +5,8 @@ from projects.models import Project
 
 class ProjectSerializer(serializers.HyperlinkedModelSerializer):
     owner = serializers.HyperlinkedIdentityField(source='owner.user', view_name='stitcher-detail')
+    type_display = serializers.CharField(source='get_type_display', read_only=True)
 
     class Meta:
         model = Project
-        fields = ['id', 'title', 'description', 'type', 'owner']
+        fields = ['id', 'title', 'description', 'type', 'type_display', 'max_stitches', 'owner']
